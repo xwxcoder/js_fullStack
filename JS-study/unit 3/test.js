190649ae622195749d59865bcde5957eb76a6810
xwxcoder <1213698738@qq.com>
2019-10-18 00:00:40 +0800

    jiayou

---
+++ JS-study/unit 3/test.js
@@ -0,0 +1,64 @@
+// var message = "hi";
+// // alert(message);
+// console.log(message);
+
+// function test1() {
+//   var message = "hi";
+// }
+// test1();
+// console.log(message);
+
+// function test2() {
+//   message = "hi";
+// }
+// test2();
+// console.log(message);
+// var message;
+// var message1 = "some string";
+// var message2 = 99;
+// var message3 = true;
+// var message4 = function(){};
+
+// console.log(message);
+// console.log(typeof message1);
+// console.log(typeof message2);
+// console.log(typeof (message3));
+// console.log(typeof message4);
+// console.log(typeof null);
+
+
+// var message;
+// console.log(typeof message);
+// console.log(typeof message1);
+
+// var message = null;
+// console.log(typeof message);
+
+// var found = true;
+// var lost = false;
+// var message = "Hello World!";
+// var messageAsBoolean = Boolean(message);
+// console.log(found);
+// console.log(lost);
+// console.log(message);
+// console.log(messageAsBoolean);
+// if(message) {
+//   console.log("value is true");
+// }
+
+// var num1 = 55;    //整数 55
+// var num2 = 070;   //八进制的56
+// var num3 = 079;   //无效的八进制数值--解析为79
+// var num4 = 08;    //无效的八进制数值--解析为8
+// var num5 = 0xA;   //十六进制的10
+// var num6 = 0x1f;  //十六进制的31
+
+// console.log(num1);
+// console.log(num2);
+// console.log(num3);
+// console.log(num4);
+// console.log(num5);
+// console.log(num6);
+
+
+
---
+++ lol/readme.md
@@ -0,0 +1,14 @@
+- 可以使用html表达的一定不要用图表达
+- inline 元素不能直接设置宽与高
+  display:inline-block
+- 节日活动页面
+- bg-jpg 是雪碧图 sprites
+  网页打开速度 ，一切
+  速度杀手是 http 请求数
+  img src
+  link href
+  script src
+  http 请求去下载
+  html 这外，
+  并发http请求数是有限的
+  - html css 积累类名
